ee7c674fdf9f4c2b0f12291f6cb05e478fe1a175
cholponesn <cholponkaimova653@example.com>
2021-06-30 18:13:20 +0600

    add urls

--- StomCentr/signup/urls.py
+++ StomCentr/signup/urls.py
@@ -3,6 +3,24 @@ from .views import *
 
 urlpatterns = [
     path('',DayViewSet.as_view({
-                                'get':'list'}))
+                                'get':'list',
+    'post':'create',}),name='day'),
+    path('day/<int:day_id>/', DayViewSet.as_view({
+        'get':'retrieve',
+        'put':'update',
+        'delete':'destroy'
+    }),name='day_detail'),
+    path('work_days/',DoctorDayView.as_view({
+        'get':'list'})),
+    path('work_days/<int:day_id>/', DoctorDayView.as_view({
+        'post':'create'
+
+        })),
+    path('modify_wd/<int:doctor_day_id/',DoctorDayView.as_view({
+        'put':'update',
+        'get':'retrieve'
+
+    }))
+
 
 ]
