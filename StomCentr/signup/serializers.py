dfac45dd5915ab5acf429be3ef046c1a646e84ef
cholponesn <cholponkaimova653@example.com>
2021-06-30 18:12:15 +0600

    mew serializer

--- StomCentr/signup/serializers.py
+++ StomCentr/signup/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
-class DaySerializer(serializers.Serializer):
+class DaySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Day
+        fields = "__all__"
+
+class DoctorDaySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DoctorDay
         fields = "__all__"
