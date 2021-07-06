094298d2e3538ce320a3fb4475036fb2f1c672c4
cholponesn <cholponkaimova653@example.com>
2021-07-06 17:30:23 +0600

    add class order, doctor

--- StomCentr/signup/views.py
+++ StomCentr/signup/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from .serializers import *
-from .permissions import IsSuperUser, DoctorDayPermission
+from .permissions import *
 
 class DayViewSet(ViewSet):
     serializer_class = DaySerializer
@@ -64,3 +64,87 @@ class DoctorDayView(ViewSet):
         work_day = DoctorDay.objects.get(id=doctor_day_id)
         serializer = DoctorDaySerializer(work_day)
         return Response(serializer.data)
+
+class OrderViewSet(ViewSet):
+    permission_classes = [ClientPermission]
+    serializer_class = OrderSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        client = request.user
+        if serializer.is_valid():
+            doctor_id = serializer.data.get('doctor')
+            day_id = serializer.data.get('day')
+            try:
+                work_day = DoctorDay.objects.get(doctor=doctor_id, day=day_id)
+                day = Day.objects.get(id=day_id)
+                doctor = User.objects.get(id=doctor_id)
+                if work_day.status == 'free':
+                    order = Order.objects.create(client=client, doctor=doctor, day=day)
+                    work_day.status = 'reserved'
+                    work_day.save()
+                    return Response(serializer.data, status=201)
+                return Response('Это время уже занято!', 400)
+            except DoctorDay.DoesNotExist:
+                return Response('Врач в такой день не работает!', status=404)
+        return Response(serializer.errors, status=400)
+
+    def list(self, request):
+        group = request.user.groups.all()[0].name
+        user = request.user
+        if group == 'client':
+            orders = Order.objects.filter(client=user)
+        elif group == 'doctor':
+            orders = Order.objects.filter(doctor=user)
+        serializer = OrderDisplaySerializer(orders, many=True)
+        return Response(serializer.data)
+
+
+    def update(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        serializer = self.serializer_class(order, data=request.data)
+        if serializer.is_valid():
+            day_id = request.data.get('day')
+            doctor_id = request.data.get('doctor')
+            try:
+                dd = DoctorDay.objects.get(day=day_id, doctor=doctor_id,
+                                           status='free')  # SELECT * FROM DOCTORDAY WHERE DAY=DAY_ID AND DOCTOR=DOCTOR_ID AND STATUS=FREE
+                dd.status = 'reserved'
+                dd.save()
+            except DoctorDay.DoesNotExist:
+                return Response('доктор в этот день не работает или день забит выберите другой!')
+            serializer.save()
+            return Response(serializer.data, status=202)
+        return Response(serializer.errors, status=400)
+
+    def retrieve(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        serializer = self.serializer_class(order)
+        return Response(serializer.data, status=200)
+
+    def destroy(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        o_id = order.id
+        order.delete()
+        return Response(f'{o_id} successfully deleted!', status=204)
+
+class DoctorViewSet(ViewSet):
+    serializer_class = UserListSerializer
+    permission_classes = [DoctorPermission,]
+
+    def list(self,request):
+        doctors = User.objects.filter(groups__name='doctor')
+        serializer = self.serializer_class(doctors,many=True)
+        return Response(serializer.data)
+
+    def retrieve(self,request,d_username):
+        doctor = User.objects.get(username=d_username)
+        serializer = UserDetailSerializer(doctor)
+        return Response(serializer.data)
+
+    def updata(self,request,d_username):
+        pass
+    def destroy(self,request,d_username):
+        pass
+
+
