c8675347f9afdcc5deddda52dbec8359b1035ed3
cholponesn <cholponkaimova653@example.com>
2021-07-06 17:28:12 +0600

    add model order

--- StomCentr/signup/models.py
+++ StomCentr/signup/models.py
@@ -10,6 +10,16 @@ class Day(models.Model):
 class DoctorDay(models.Model):
     doctor = models.ForeignKey(User,on_delete=models.CASCADE)
     day = models.ForeignKey(Day,on_delete=models.CASCADE)
+    status = models.CharField(max_length=15, choices=(
+        ('free', 'free'),
+        ('reserved', 'reserved')
+    ), default='free')
+
+class Order(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clients')
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doctors')
+    date_created = models.DateTimeField(auto_now_add=True)
 
 
 
