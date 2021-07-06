0c194e4884dd13c7353728db00200a39dcba1787
cholponesn <cholponkaimova653@example.com>
2021-07-06 17:30:37 +0600

    new serializer

--- StomCentr/signup/serializers.py
+++ StomCentr/signup/serializers.py
@@ -12,3 +12,37 @@ class DoctorDaySerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorDay
         fields = "__all__"
+
+class DoctorDayDisplaySerializer(serializers.ModelSerializer):
+    doctor = serializers.StringRelatedField()
+    day = serializers.StringRelatedField()
+
+    class Meta:
+        model = DoctorDay
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['day', 'doctor']
+
+
+class OrderDisplaySerializer(serializers.ModelSerializer):
+    client = serializers.StringRelatedField()
+    day = serializers.StringRelatedField()
+    doctor = serializers.StringRelatedField()
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','groups']
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
